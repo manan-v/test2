a983989fb63ebb0c8feee9c3ae8acd852459c1b2
Parth <parthmshah1302@gmail.com>
2022-05-18 21:14:05 +0530

    done: replaceURL (except membersURL) complete with api_request

--- getSpecificOrgDetails.py
+++ getSpecificOrgDetails.py
@@ -1,3 +1,4 @@
+from operator import or_
 import requests
 import json
 import argparse
@@ -5,22 +6,21 @@ from time import sleep
 from tqdm import tqdm
 from helper_methods import *
 
-
 # Sample: python3 getSpecificOrgDetails.py --config_file project.config --org_name novelys --output_file githubSpecificOrgDetails.json
 reqUrl = "https://api.github.com/orgs/"
 
+# Parse the arguements
 parser = argparse.ArgumentParser()
 parser.add_argument('--config_file')
 parser.add_argument('--org_name')
 parser.add_argument('--output_file')
-
 args = vars(parser.parse_args())
 
+# Get the API key to use
 apiDeque = apiRobin.parseConfig(args['config_file'])
-
 headers = getAPIToken(apiDeque)
 
-
+# Request an organisation name and return the response
 def getSpecificOrgDetails(orgName, headers):
     response = requests.get(reqUrl + str(orgName), headers=headers).json()
     if 'message' not in response:
@@ -28,26 +28,7 @@ def getSpecificOrgDetails(orgName, headers):
     else:
         return None
 
-def replaceURL(parentJSON):
-    counter=1
-    totCounter=1
-    excludeUrl=['avatar','html']
-    with open(parentJSON, 'r+') as file:
-        content = json.loads(file.read())
-        # for urlField, reqUrl in content.items():
-        #     if "url" in urlField and not any(exclude in urlField for exclude in excludeUrl) and len(urlField) > 3:
-        #         # print("totcounter is:"+str(totCounter))
-        #         totCounter+=1
-        #         print(urlField)
-                # response = requests.get(reqUrl, headers=headers).json()
-                # with open('test2.json', 'a') as outfile:
-                #     if 'message' not in response and response is not None:
-                #         json.dump(response, outfile)
-                #         print("counter is:"+str(counter))
-                #         counter+=1
-                #         print("You made it "+urlField)
-
-
+# Get the JSON file with URLs
 with open(args['output_file'], 'a') as outfile:
     # GitHub orgs are stored with IDs from (1-n)
     while True:
@@ -60,7 +41,7 @@ with open(args['output_file'], 'a') as outfile:
                 json.dump(response, outfile)
                 outfile.write('\n')
             break  # If no exception occurred
-        # Need to replace with the exact rate limit exception
+        # Switch token when limit exhausted
         except Exception as e:
             if e.code == 403:
                 print("** SWITCHING TOKEN NOW **")
@@ -69,4 +50,51 @@ with open(args['output_file'], 'a') as outfile:
                 print("** SLEEPING FOR 1 HR **")
                 sleep(3600)
 
-replaceURL("json\parent.json")
+# Request each URL and return responses as list
+def getResponseFromURL(parentJSON, excludeUrl):
+    # Initialise empty lists to store keys, oldValue and newValue
+    keyToBeReplaced=[]
+    oldValue=[]
+    newValue = []
+    with open(parentJSON, 'r+') as parent:
+        parentContent=json.loads(parent.read())
+        # Gets the list of keys and old values
+        for key, value in parentContent.items():
+            # Append only if the key is absent in excludeUrl list
+            if "url" in key and not any(exclude in key for exclude in excludeUrl):
+                keyToBeReplaced.append(key)
+                oldValue.append(value)
+                # TODO Add try except statement to switch API post exhaustion
+                response = requests.get(value, headers=headers).json()
+                newValue.append(response)
+            
+        # Organisation's URL (for eg: https://api.github.com/orgs/novelys) is not useful
+        keyToBeReplaced.pop(0)
+        oldValue.pop(0)
+        newValue.pop(0)
+    
+    return keyToBeReplaced, oldValue, newValue
+
+# Define keys that are to be excluded while search
+excludeUrl = ['avatar', 'html', 'members']
+# Get the keys, oldValues and newValues
+keyToBeReplaced, oldValue, newValue=getResponseFromURL(args['output_file'],excludeUrl)
+
+with open(args['output_file']) as parent:
+    parentContent = json.loads(parent.read())
+    iterator=0
+    # Iterate over the entire file
+    for key, value in parentContent.items():
+        # If a key is found in the keyToBeReplaced list
+        if(key == keyToBeReplaced[iterator]):
+            # Replace the current value with the corresponding response from newValue 
+            parentContent[key]=newValue[iterator]
+            iterator=iterator+1
+            # Prevents index out of bounds error
+            if(iterator==len(keyToBeReplaced)):
+                break
+with open(args['output_file'],'w+') as parent: 
+    # Rewrite the JSON with the updated data
+    parentContent=json.dumps(parentContent)
+    parent.write(parentContent)
+print(" ** URLs REPLACED FOR ORG '"+args['org_name']+"' IN FILE : '"+args['output_file']+"' **")
--- helper_methods.py
+++ helper_methods.py
@@ -1,11 +1,12 @@
 import apiRobin
+import json
 
 def getAPIToken(apiDeque):
     # Rotate the API queue
     apiDeque = apiRobin.rotateAPI(apiDeque)
 
     # Create the new header
-    # headers_new = {'Authorization': 'token ' + apiDeque[0]}
+    headers_new = {'Authorization': 'token ' + apiDeque[0]}
     print(" ** TOKEN SELECTED: {} **".format(headers_new))
     return headers_new
 
@@ -17,3 +18,31 @@ def getRandomAPIToken(apiDeque):
     headers_new = {'Authorization': 'token ' + apiDeque[0]}
     # print(" ** TOKEN SELECTED: {} **".format(headers_new))
     return headers_new
+
+
+def replaceURL(parentJSON, childJSON):
+    # counter = 1
+    # totCounter = 1
+    excludeUrl = ['avatar', 'html']
+    keyToBeReplaced=[]
+    with open(parentJSON, 'r+') as parent:
+        parentContent = json.loads(parent.read())
+        for urlField, reqUrl in parentContent.items():
+            if "url" in urlField and not any(exclude in urlField for exclude in excludeUrl) and len(urlField) > 3:
+                print(urlField+" - "+reqUrl)
+                keyToBeReplaced.append(urlField)
+        # response = requests.get(reqUrl, headers=headers).json()
+        # FIXME - replace harcoded response with requested response
+    # keyToBeReplaced = 'repos_url'
+    with open(childJSON, 'r+') as child:
+        response = json.loads(child.read())
+    for key, value in parentContent.items():
+        if key == keyToBeReplaced[1]:
+            parentContent[key] = response
+            with open(parentJSON, 'w+') as parent:
+                parentContent = json.dumps(parentContent)
+                parent.write(parentContent)
+                # print(parentContent[keyToBeReplaced])
+
+            # Pass
+    # print(parentContent[keyToBeReplaced])
