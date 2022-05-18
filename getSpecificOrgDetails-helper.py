4e54e0d35f67f551b6e389f2162254f0f3d4abd9
Parth <parthmshah1302@gmail.com>
2022-05-18 22:09:15 +0530

    added: replaceURL over top-repo list

--- getSpecificOrgDetails-helper.py
+++ getSpecificOrgDetails-helper.py
@@ -1,6 +1,16 @@
 from helper_methods import * 
+import time, math
 
 # NOTE: To run the code - delete getSpecificOrgDetailsData/novelys.json if already present
 # Sample: python3 getSpecificOrgDetails-helper.py 
 
-replaceURL('novelys','project.config','getSpecificOrgDetailsData/novelys.json')
+start=time.time()
+
+with open('top-repo') as orgs:
+    for line in orgs:
+        orgName, number = line.strip().split(None, 1)
+        replaceURL(orgName,'project.config','getSpecificOrgDetailsData/'+orgName+'.json')
+
+end=time.time()
+
+print(" ** THE EXECUTION TOOK: "+str(math.floor(end-start))+" SECONDS **")
