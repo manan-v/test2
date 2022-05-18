410807cda98e3ec39c4e306afe32da11e6682988
Parth <parthmshah1302@gmail.com>
2022-05-19 01:52:20 +0530

    chore: re-attempt untracking git lfs

--- getSpecificOrgDetails-module.py
+++ getSpecificOrgDetails-module.py
@@ -3,7 +3,7 @@ import time, math
 
 # NOTE: To run the code - delete getSpecificOrgDetailsData/novelys.json if already present
 #       else: You may recieve 'json.decoder.JSONDecodeError: Extra data:' error
-# Also, make sure there is a directory named getSpecificOrgDetailsData on the same level
+# Also, make sure there is a directory named getSpecificOrgDetailsData on the same
 # Sample: python3 getSpecificOrgDetails-module.py 
 
 start=time.time()
