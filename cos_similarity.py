dbe43b15d94e509c024bb000b4c8368a998263ac
Parth <parthmshah1302@gmail.com>
2022-07-11 12:43:20 +0530

    update: graph for cos_similarity+corrected user_list for both similarity

--- cos_similarity.py
+++ cos_similarity.py
@@ -2,6 +2,9 @@ import networkx as nx
 import networkx_addon
 import numpy as np
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import time
 start = time.time()
 
@@ -9,8 +12,8 @@ start = time.time()
 def read_org_graph(orgName):
     G = nx.read_gml("graphs/gmlFiles/"+orgName+".gml", label='id')
     type = nx.get_node_attributes(G, "bipartite")
-    user_nodes = [node for node in G.nodes if type[node] == 0]
-    # repo_nodes = [node for node in G.nodes if type[node] == 1]
+    user_nodes = [node for node in G.nodes if type[node] == 1]
+    # repo_nodes = [node for node in G.nodes if type[node] == 0]
 
     user_G = nx.Graph(G.subgraph(user_nodes))
     # repo_G = nx.Graph(G.subgraph(repo_nodes))
@@ -24,13 +27,14 @@ def read_org_graph(orgName):
 def fill_inner_matrix(source, user_list):
     inner_matrix = []
     for i in range(len(user_list)):
-        print("(source, target) is: ("+str(source)+", "+str(i)+")")
         try:
-            cosine_sim = all_cosine_sim[source][i]
+            target=user_list[i]
+            # print("(source, target) is: ("+str(source)+", "+str(target)+")")
+            cosine_sim = all_cosine_sim[source][target]
         except KeyError:
             cosine_sim = 1.0
         inner_matrix.append(cosine_sim)
-        if(i == 10):
+        if(i == 100):
             break
     return inner_matrix
 
@@ -39,19 +43,26 @@ def fill_outer_matrix(G, user_list):
     outer_matrix = []
     for i in range(len(user_list)):
         # print("source is: "+str(i))
-        outer_matrix.append(fill_inner_matrix(i, user_list))
-        if(i == 10):
+        outer_matrix.append(fill_inner_matrix(user_list[i], user_list))
+        if(i == 100):
             break
     return outer_matrix
 
+def show_heatmap(matrix):
+    ax = sns.heatmap(matrix, linewidth=0.5, cmap="YlGnBu")
+    plt.show()
 
 G, user_nodes, user_list = read_org_graph("salesforce")
 all_cosine_sim = networkx_addon.similarity.cosine(G)
+print(len(user_list))
 cosine_similarity_matrix = np.asarray(fill_outer_matrix(G, user_list))
 print(cosine_similarity_matrix)
 
 np.savetxt("similarity_matrix/cos_salesforce.csv",
            cosine_similarity_matrix, delimiter=",")
+
+show_plt(cosine_similarity_matrix)
+plt.clf()
 
 end = time.time()
 print("Time taken: "+str(round(end-start))+" sec")
--- simrank.py
+++ simrank.py
@@ -1,5 +1,9 @@
 import networkx as nx
 import numpy as np
+import networkx_addon
+
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import time
 start = time.time()
@@ -8,8 +12,8 @@ start = time.time()
 def read_org_graph(orgName):
     G = nx.read_gml("graphs/gmlFiles/"+orgName+".gml", label='id')
     type = nx.get_node_attributes(G, "bipartite")
-    user_nodes = [node for node in G.nodes if type[node] == 0]
-    # repo_nodes = [node for node in G.nodes if type[node] == 1]
+    user_nodes = [node for node in G.nodes if type[node] == 1]
+    # repo_nodes = [node for node in G.nodes if type[node] == 0]
 
     user_G = nx.Graph(G.subgraph(user_nodes))
     # repo_G = nx.Graph(G.subgraph(repo_nodes))
@@ -19,32 +23,55 @@ def read_org_graph(orgName):
         user_list.append(key)
     return G, user_nodes, user_list
 
+def calculate_similarity(source, target):
+    simrank=nx.simrank_similarity(G, source, target)
+    return simrank
 
-def fill_inner_matrix(G, source, user_list):
-    inner_matrix = []
+def fill_inner_matrix(G, source, user_list,source_index,matrix):
+    # inner_matrix = []
     for i in range(len(user_list)):
-        print("(source, target) is: ("+str(source)+", "+str(i)+")")
-        inner_matrix.append(nx.simrank_similarity(G, source, user_list[i]))
-        if(i == 3):
+        target=user_list[i]
+        target_index=i
+
+        # print("(source, target) is: ("+str(source)+", "+str(target)+")")
+        # print("source_index: "+str(source_index))
+        # print("target_index: "+str(target_index))
+
+        # inner_matrix.append(s[source][target])
+
+        matrix[source_index][target_index]=calculate_similarity(source,target)
+        print(matrix[source_index][target_index])
+        if(i == 1):
             break
-    return inner_matrix
+    return matrix
 
 
-def fill_outer_matrix(G, user_list):
-    outer_matrix = []
+def fill_sim_matrix(G, user_list):
+    sim_matrix = [[0]*len(user_list)]*len(user_list)
     for i in range(len(user_list)):
         # print("source is: "+str(i))
-        outer_matrix.append(fill_inner_matrix(G, i, user_list))
-        if(i == 3):
+        # sim_matrix.append(fill_inner_matrix(G, source=user_list[i], user_list=user_list,source_index=i))
+        sim_matrix = fill_inner_matrix(G, source=user_list[i], source_index=i, user_list=user_list, matrix=sim_matrix)
+        if(i == 1):
             break
-    return outer_matrix
+    return sim_matrix
 
+
+def show_heatmap(matrix):
+    ax = sns.heatmap(matrix, linewidth=0.5, cmap="YlGnBu")
+    plt.show()
 
 G, user_nodes, user_list = read_org_graph("salesforce")
-outer_matrix = fill_outer_matrix(G, user_list)
+# s = networkx_addon.similarity.simrank(G)
+# print("here")
+outer_matrix = fill_sim_matrix(G, user_list)
+# print(type(outer_matrix))
 similarity_matrix = np.asarray(outer_matrix)
+# print(type(similarity_matrix))
 
-np.savetxt("similarity_matrix/sim_salesforce.csv",
+# show_heatmap(similarity_matrix)
+
+np.savetxt("similarity_matrix/simrank_salesforce.csv",
            similarity_matrix, delimiter=",")
 
 end = time.time()
