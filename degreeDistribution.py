dbf5b87345b1db8d172f5c5b1b0aa66f408c1a6d
Parth <parthmshah1302@gmail.com>
2022-07-26 15:30:37 +0530

    added: P1, P2, P3, P4

--- degreeDistribution.py
+++ degreeDistribution.py
@@ -3,10 +3,8 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-from joblib import Parallel, delayed
-import multiprocessing
 import time
-from helper_methods import orgViz
+start=time.time()
 import logging
 
 logging.basicConfig(
@@ -14,78 +12,73 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-start = time.time()
-num_cores = multiprocessing.cpu_count()-1
-# print('Total usable number of cores: {}'.format(num_cores))
 
-# First parameter is orgName
-# Add the other parameters only if source and dest folders are different
+def plotGraph(orgName, userDegree, repoDegree, G, dest='plots/'):
+    
+    if not os.path.exists(dest+orgName):
+        os.makedirs(dest+orgName)
+
+    # Organisation
+    X, Y = nx.bipartite.sets(G)
+    pos = dict()
+    pos.update((n, (1, i))
+               for i, n in enumerate(X))  # put nodes from X at x=1
+    pos.update((n, (2, i))
+               for i, n in enumerate(Y))  # put nodes from Y at x=2
+    nx.draw(G, pos=pos, with_labels=True)
+    # plt.show()
+    plt.savefig(dest+orgName+'/'+orgName+'_bipartite.png')
+    plt.clf()
+    
+    # P1
+    x,y=np.unique(repoDegree,return_counts=True)
+    plt.plot(x,y,'o-')
+    plt.xlabel("Number of repos")
+    plt.ylabel("Degree")
+    plt.title("P1: Degree distribution (B_w) for repos: "+orgName)
+    # plt.show()
+    plt.savefig(dest+orgName+'/'+orgName+'_P1.png')
+    plt.clf()
+    
+    # P2
+    x, y = np.unique(userDegree, return_counts=True)
+    plt.plot(x, y, 'o-')
+    plt.xlabel("Number of users")
+    plt.ylabel("Degree")
+    plt.title("P2: Degree distribution (B_w) for users: "+orgName)
+    # plt.show()
+    plt.savefig(dest+orgName+'/'+orgName+'_P2.png')
+    plt.clf()
 
 
-def calcDegreeDist(orgName, source="graphs/gmlFiles/", dest="rudi-analysis/degreeDistribution/"):
+def calcDegreeDist(orgName, source="graphs/gmlFiles/", dest="gml/user-repo-GML/"):
     try:
-        G = nx.read_gml(source+orgName+".gml", label='id')
-
-        degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
-
-        # Get those with degree > 1 - Comment when computing raw
-        degree_sequence = [i for i in degree_sequence if degree_sequence.count(i) > 1]
-
-        dmax = max(degree_sequence)
-
-        fig = plt.figure("Degree of a random graph", figsize=(8, 8))
-
-        # Create a gridspec for adding subplots of different sizes
-        axgrid = fig.add_gridspec(5, 4)
-
-        ax0 = fig.add_subplot(axgrid[0:3, :])
-        Gcc = G.subgraph(
-            sorted(nx.connected_components(G), key=len, reverse=True)[0])
-        pos = nx.spring_layout(Gcc, seed=10396953)
-        nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20,with_labels=True)
-        nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
-        ax0.set_title("Connected components in organisation "+orgName+"")
-        ax0.set_axis_off()
-
-        ax1 = fig.add_subplot(axgrid[3:, :2])
-        ax1.plot(degree_sequence, "b-", marker="o")
-        ax1.set_title("Degree Rank Plot")
-        ax1.set_ylabel("Degree")
-        ax1.set_xlabel("Rank")
-
-        ax2 = fig.add_subplot(axgrid[3:, 2:])
-        ax2.bar(*np.unique(degree_sequence, return_counts=True))
-        ax2.set_title("Degree histogram")
-        ax2.set_xlabel("Degree")
-        ax2.set_ylabel("# of Nodes")
-
-        fig.tight_layout()
-        fig.savefig(dest+orgName+".png")
-        plt.clf()
-
+        G = nx.read_gml(source+orgName+".gml", label='label')
+        users = [x for x, y in G.nodes(data=True) if y['bipartite'] == 0]
+        repos = [x for x, y in G.nodes(data=True) if y['bipartite'] == 1]
+        
+        repoDegree=[]
+        userDegree=[]
+        for repo in repos:
+            repoDegree.append(int(G.degree(repo)))
+        for user in users:
+            userDegree.append(G.degree(user))
+        
+        # print(repoDegree)
+        # print(userDegree)
+        return G, userDegree, repoDegree
     except Exception as e:
+        print("errr")
         logger.error(str("Error for org "+orgName+": "+str(e)))
+    
 
-
-destFolder = "rudi-analysis/degreeDistribution/nonSingularOccurence/"
-
-# orgList = os.listdir('graphs/gmlFiles/')
-# # Strip extension .gml from orgName
-# orgList = [x.split('.')[0] for x in orgList]
-# orgList.sort()
-
+org='dummy'
+destFolder = "gml/user-repo-GML/"
 logger.info(str("Computing sequentially"))
-# for org in orgList:
-#     calcDegreeDist(orgName=org, dest=destFolder)
-calcDegreeDist(orgName='dummy', source='gml/user-repo-GML/',dest='gml/user-repo-GML/')
-#     break
 
-# logger.info(str("Computing parallely"))
-# Parallel(n_jobs=num_cores)(delayed(calcDegreeDist)(org) for org in orgList)
+G,userDegree, repoDegree =calcDegreeDist(orgName=org, source='gml/user-repo-GML/',dest=destFolder)
 
-# print(str("Computing sequentially"))
-# for orgName in orgList:
-#     orgViz(orgName)
+plotGraph(org, userDegree, repoDegree, G)
 
 end = time.time()
-logger.info(str("Computing completed in "+str(round(end-start))+" seconds"))
+logger.info(str("Computing completed in "+str(round(end-start))+" seconds for "+org))
