0971f816e4787949eb9bfd41129a967d282a2006
Parth <parthmshah1302@gmail.com>
2022-07-26 22:45:48 +0530

    added: distribution plot for org vs noOfUsers

--- contriDistributor.py
+++ contriDistributor.py
@@ -1,12 +1,21 @@
-import json, os
+import json
+import os
+import matplotlib.pyplot as plt
 
 orgList = os.listdir('starredAndSub/orgJSON/starred/')
 contriDict = {}
-for org in orgList: 
-    print(org)
-    with open('starredAndSub/orgJSON/starred/'+org,"r") as f: 
-        orgDict=json.load(f)
-
-    contriDict[org]=len(orgDict.keys())
+numOfUser = []
+for org in orgList:
+    with open('starredAndSub/orgJSON/starred/'+org, "r") as f:
+        orgDict = json.load(f)
+    contriDict[org] = len(orgDict.keys())
+    numOfUser.append(len(orgDict.keys()))
 with open('starredAndSub/orgJSON/userDict.json', 'w') as f:
     json.dump(contriDict, f)
+
+values, bins, bars = plt.hist(numOfUser, 30, edgecolor='white')
+plt.title('Distribution of Org vs Num of Users')
+plt.xlabel('Number of Users')
+plt.ylabel('Number of Orgs')
+plt.bar_label(bars, fontsize=8, color='black')
+plt.savefig('starredAndSub/orgJSON/userDict.png')
