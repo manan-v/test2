e10bab77f7b2c9a9fc1366940546ecff30e3fa3e
Parth <parthmshah1302@gmail.com>
2022-08-19 18:07:02 +0530

    feat: update data instead of dumping at end

--- step2_obtainMemberActivity/getRepoForContributor.py
+++ step2_obtainMemberActivity/getRepoForContributor.py
@@ -1,4 +1,3 @@
-import createStarredAndSub
 import requests
 import json 
 
@@ -11,6 +10,17 @@ from helper_methods import getRandomAPIToken
 
 import time
 start=time.time()
+
+def getContributorList(orgName, repo_details_dir='../step1_obtainRepoDetails/data/repo_details/'):
+    with open(repo_details_dir+orgName+'.json') as f:
+        data = json.load(f)
+    contributorList = []
+    for repo in data['repoDetails']:
+        for contributorUrl in repo['contributors']:
+            if contributorUrl['login'] not in contributorList:
+                contributorList.append(contributorUrl['login'])
+    contributorList.sort()
+    return contributorList
 
 def getFullJSON(contributor, activityType):
     apiDeque = apiRobin.parseConfig('../project.config')
@@ -25,7 +35,7 @@ def getFullJSON(contributor, activityType):
             break
         fullList.extend(List)
         counter = counter+1
-    json.dump(fullList, open('data/test/'+contributor+'.json', 'w'))
+    # json.dump(fullList, open('data/test/'+contributor+'.json', 'w'))
     return fullList
 
 def filterJSON(unfilteredList):
@@ -39,14 +49,19 @@ def filterJSON(unfilteredList):
             nodeIDList.append(item['node_id'])
     return nodeIDList
         
-orgList=['yeebase']
-path='data/test/'
-activityList=['starred']
-for org in orgList:
-    contributorList=createStarredAndSub.getContributorList(org)
-    reqCounter=0
-    for contributor in contributorList:
-        for activity in activityList:
-            fullList=getFullJSON(contributor, activity)
-            nodeIDList=filterJSON(fullList)
-            print(contributor,nodeIDList)
+def createStarredAndSub(orgList):
+    path='data/test/'
+    activityList=['starred','subscriptions']
+    for org in orgList:
+        contributorList=getContributorList(org)
+        orgData={}
+        for contributor in contributorList:
+            orgData[contributor] = {}
+            for activity in activityList:
+                orgData[contributor][activity] = filterJSON(getFullJSON(contributor, activity))
+                print(contributor,activity)
+            json.dump(orgData,open('data/test/'+org+'.json','r+'))
+
+orgList = ['yeebase']
+createStarredAndSub(orgList)
+print("Time taken: ",round(time.time()-start))
